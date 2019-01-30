587eafd23271a434fce58a18ed2838a3961b9ea9
j-prosser <jp2415@ic.ac.uk>
2019-01-30 04:04:38 +0000

    changed pt resolution

--- src/utils.cc
+++ src/utils.cc
@@ -17,8 +17,8 @@ float abs_diff_pt(HGCC3D& cluster, HGCgen& truth) {
 ResolutionStats CalculateStats(HGCC3D& cluster, HGCgen* truth, float radius) {
 	ResolutionStats result = ResolutionStats(); 
 	result.setRadius(radius);
-	// Pt res: 
-	result.setPtRes( cluster.Pt() - truth->Pt() );
+	// Pt res: Pt^cl / Pt^gen
+	result.setPtRes( cluster.Pt() / truth->Pt() );
 	result.setXRes( cluster.xNorm() - truth->xNorm()  );
 	result.setYRes( cluster.yNorm() - truth->yNorm() ); 
 	result.setRRes( sqrt(result.XRes()*result.XRes() + result.YRes()*result.YRes()) );
